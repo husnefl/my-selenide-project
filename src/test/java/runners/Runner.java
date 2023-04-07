27042344640b8b3551f8f1b5525d1161190b37a2
husnefl <husneakincioglu@gmail.com>
2023-04-07 09:10:33 -0400

    nokta

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions"},
         dryRun = false,
-        tags = "@navigation"
+        tags = "@navigations"
 
 )
 public class Runner {
